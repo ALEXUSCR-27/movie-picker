08346a64b0dede98a7b496d10604453b8ce845e6
Alex Sánchez C <asmalexander@hotmail.com>
2024-12-08 23:59:47 -0600

    Add Next and tailwind configuration

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'm.media-amazon.com',
+          port: '',
+          pathname: '/images/**',
+          search: '',
+        },
+      ],
+    },
+  }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      height: {
+        'card': '55%',
+      }
     },
   },
   plugins: [],
