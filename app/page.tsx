7c312a2774a13ce641827c9099513d20a51f902e
Alex Sánchez C <asmalexander@hotmail.com>
2024-12-10 14:23:43 -0600

    Add SearchBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+"use client"
+
 import CardsContainer from "./components/CardsContainer";
+import SearchBar from "./components/SearchBar";
+
 export default function Home() {
   return (
     <div>
         <main>
-            <div className="p-10 w-screen h-screen">
-                <div className="w-full h-full ">
+            <div className="p-5 w-full h-full content-center items-center">
+                <div className="p-5">
+                    <SearchBar/>
+                </div>
+                <div>
                     <CardsContainer data={{title:"Toy Story"}}/>
                 </div>
             </div>
