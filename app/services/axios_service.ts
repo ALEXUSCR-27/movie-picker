8a971a540170b51fd21306c17932ca1a658193c3
Alex Sánchez C <asmalexander@hotmail.com>
2024-12-16 20:17:20 -0600

    Add axios service

---
+++ app/services/axios_service.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const axiosService = axios.create({
+    baseURL: 'http://127.0.0.1:5000', 
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+axiosService.interceptors.request.use (
+    (response) => {
+        return response;
+    },
+    (error) => {
+        alert("ERROR => "+error)
+        return Promise.reject(error);
+    }
+)
+export default axiosService;
