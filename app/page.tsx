7b6422c72be34e352ddf6a3d57c60b5930c5ffeb
Alex Sánchez C <asmalexander@hotmail.com>
2024-12-16 20:45:42 -0600

    Add functionality to set data to state

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,29 @@
 "use client"
 
+import { useState } from "react";
 import CardsContainer from "./components/CardsContainer";
 import SearchBar from "./components/SearchBar";
+import { card_data } from "./types/params";
 
 export default function Home() {
-  return (
-    <div>
-        <main>
-            <div className="p-5 w-full h-full content-center items-center">
-                <div className="p-5">
-                    <SearchBar/>
+    const [data, setData] = useState(Array<card_data>);
+
+    const set_data_results = ({data_results}: { data_results: card_data[] }) => {
+        setData(data_results);
+    }
+
+    return (
+        <div>
+            <main>
+                <div className="p-5 w-full h-full content-center items-center">
+                    <div className="p-5">
+                        <SearchBar set_data_results={set_data_results}/>
+                    </div>
+                    <div>
+                        <CardsContainer data={data}/>
+                    </div>
                 </div>
-                <div>
-                    <CardsContainer data={{title:"Toy Story"}}/>
-                </div>
-            </div>
-        </main>
-    </div>
-  );
+            </main>
+        </div>
+    );
 }
