5babae61ee8f8a615206dcf9a2d611b95b9eb5b7
Alex Sánchez C <asmalexander@hotmail.com>
2024-12-22 22:04:24 -0600

    Refactor background desing

--- app/components/CardsContainer.tsx
+++ app/components/CardsContainer.tsx
@@ -1,3 +1,4 @@
+import { url } from "inspector";
 import { card_data } from "../types/params";
 import Card from "./Card";
 // import test from "../../public/test.json"
@@ -6,17 +7,18 @@ export default function CardsContainer({data}: { data: Array<card_data> }) {
     //const dataTemp = test.titleResults.results;
 
     return (
-        <div className="p-5 flex flex-row gap-3 w-full h-1/3 justify-start border-gray-800 border rounded">
-            <div className=" w-1/6 h-full">
+        <div className="flex flex-row gap-3 w-full min-h-[52rem] max-h-[52rem] h-1/3 justify-start border-zinc-600 border rounded">
+            <div className="p-3 w-1/6 h-full">
                 Seccion izquierda
             </div>
-            <div className="rounded-sm border-slate-900 border w-full h-full grid grid-cols-6 gap-x-0 gap-y-4 overflow-y-scroll overflow-x-hidden px-2 pt-2 pb-4">
+            <div className="rounded-sm  w-full grid grid-cols-6 gap-x-0 gap-y-4 overflow-y-scroll overflow-x-hidden px-2 pt-2 pb-4">
                 {data.map((row, index) => (
                     <Card
                         key={index}
                         title={row.title}
-                        rating={row.release_year}
-                        url_image={row.image_url}
+                        rating={row.rating}
+                        release_date={row.release_year}
+                        url_image={row.image_url || 'vercel.svg'}
                     >
                     </Card>
 
