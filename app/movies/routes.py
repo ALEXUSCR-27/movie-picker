1c970d7a1d52b0f9d0b7f3824a56411caef9d64c
Alex Sánchez C <asmalexander@hotmail.com>
2024-12-22 18:11:29 -0600

    Refactor blueprint to use new ott api

--- app/movies/routes.py
+++ app/movies/routes.py
@@ -1,25 +1,45 @@
-
 import os
 import requests
 from flask import Blueprint, jsonify, render_template, request
 
 movie_blueprint = Blueprint('movie_blueprint', __name__, template_folder='templates')
-key = os.getenv('x_rapidapi_key')
-host = os.getenv('x_rapidapi_host')
-api_url = os.getenv('FIND_URL')
-headers = {
-    "x-rapidapi-key":key,
-    "x-rapidapi-host":host
+KEY = os.getenv('x_rapidapi_key')
+HOST = os.getenv('x_rapidapi_host')
+
+OTT_API_URL = os.getenv('OTT_ADVANCED_SEARCH_URL')
+
+REQUEST_HEADERS = {
+    "x-rapidapi-key":KEY,
+    "x-rapidapi-host":HOST
+}
+
+advanced_query = {
+    "start_year":"1970",
+    "end_year":"2024",
+    "min_imdb":"2",
+    "max_imdb":"10",
+    "genre":"action",
+    "language":"english",
+    "type":"movie",
+    "sort":"latest",
+    "page":"1"
 }
 
 @movie_blueprint.route('/search_movies', methods=['POST'])
 def search_movies():
-    movie_title = request.json['movie_title']
-    querystring = {"query":movie_title}
-    response = requests.get(api_url, headers=headers, params=querystring)
+    movie_genre = request.json['movie_title']
+    advanced_query["genre"] = movie_genre
+
+    response = requests.get(OTT_API_URL, headers=REQUEST_HEADERS, params=advanced_query)
 
     response_json = response.json()
 
-    movies_title = response_json['titleResults']['results']
 
-    return movies_title, 200
+    movies_result = response_json['results']
+    
+    
+    fixed_result = [
+        {"title": item['title'], "release_year": item['released'], "rating":item["imdbrating"], "synopsis":item["synopsis"], "image_url": item['imageurl'][0] if len(item['imageurl']) !=0  else ''} for item in movies_result
+    ]
+
+    return fixed_result, 200
